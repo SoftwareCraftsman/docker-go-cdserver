1432e4577a5aae8278f19c92305439a942e30caf
Martin Ahrer <this@martinahrer.at>
2016-04-13 11:25:33 +0200

    Added SSH keys and known_hosts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,19 @@
 
 if [ ! -f "/var/go/.ssh/id_rsa" ] ; then
   mkdir -p /var/go/.ssh
-  ssh-keygen -C "docker-gocd" -N '' -f /var/go/.ssh/id_rsa
+  ssh-keygen -C "docker-go-cdserver" -N '' -f /var/go/.ssh/id_rsa
   chown -R go:go /var/go/.ssh
   eval "$(ssh-agent -s)"
   ssh-add /var/go/.ssh/id_rsa
+  # Dump the public key so we can use it later (e.g. accessing a Git repo using ssh)
+  echo "######"
+  echo "# The go-cdserver SSH public key"
+  cat /var/go/.ssh/id_rsa.pub
+  echo "######"
+
+  #http://askubuntu.com/questions/123072/ssh-automatically-accept-keys
+  ssh-keyscan -H bitbucket.org >> /var/go/.ssh/known_hosts
+  ssh-keyscan -H github.com >> /var/go/.ssh/known_hosts
 fi
 
 if [ ! -f "/etc/go/cruise-config.xml" ] ; then
